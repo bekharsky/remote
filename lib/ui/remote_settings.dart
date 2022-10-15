1505f6b62547f941ba6be1e01ff27708b6a5e30f
sergey-bekharsky <sergey.bekharsky@supermetrics.com>
2022-10-16 00:24:47 +0300

    realign mocks

--- lib/ui/remote_settings.dart
+++ lib/ui/remote_settings.dart
@@ -14,30 +14,15 @@ class RemoteSettings extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               ListTile(
-                title: const Text('Edit'),
-                leading: const Icon(Icons.edit),
+                title: const Text('Samsung 5 Series'),
+                leading: const Icon(Icons.tv),
                 onTap: () => Navigator.of(context).pop(),
               ),
               ListTile(
-                title: const Text('Copy'),
-                leading: const Icon(Icons.content_copy),
+                title: const Text('Samsung 6 Series'),
+                leading: const Icon(Icons.tv),
                 onTap: () => Navigator.of(context).pop(),
               ),
-              ListTile(
-                title: const Text('Cut'),
-                leading: const Icon(Icons.content_cut),
-                onTap: () => Navigator.of(context).pop(),
-              ),
-              ListTile(
-                title: const Text('Move'),
-                leading: const Icon(Icons.folder_open),
-                onTap: () => Navigator.of(context).pop(),
-              ),
-              ListTile(
-                title: const Text('Delete'),
-                leading: const Icon(Icons.delete),
-                onTap: () => Navigator.of(context).pop(),
-              )
             ],
           ),
         ),
