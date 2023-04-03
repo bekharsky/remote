7797b9a8d21cd12d7ff335116a573ecaa075535b
Sergey Bekharsky <bekharsky@gmail.com>
2023-04-03 09:00:18 +0300

    add deviceinfo, device, tv

--- lib/device/tv_collector.dart
+++ lib/device/tv_collector.dart
@@ -1,6 +1,140 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:xml/xml.dart';
 import 'discover.dart';
+
+class DeviceInfo {
+  Device device;
+  String id;
+  String isSupport;
+  String name;
+  String remote;
+  String type;
+  String uri;
+  String version;
+
+  DeviceInfo({
+    required this.device,
+    required this.id,
+    required this.isSupport,
+    required this.name,
+    required this.remote,
+    required this.type,
+    required this.uri,
+    required this.version,
+  });
+
+  factory DeviceInfo.fromJson(Map<String, dynamic> json) {
+    return DeviceInfo(
+      device: Device.fromJson(json['device']),
+      id: json['id'],
+      isSupport: json['isSupport'],
+      name: json['name'],
+      remote: json['remote'],
+      type: json['type'],
+      uri: json['uri'],
+      version: json['version'],
+    );
+  }
+}
+
+class Device {
+  bool frameTVSupport;
+  bool gamePadSupport;
+  bool imeSyncedSupport;
+  String os;
+  bool tokenAuthSupport;
+  bool voiceSupport;
+  String countryCode;
+  String description;
+  String developerIP;
+  String developerMode;
+  String duid;
+  String firmwareVersion;
+  String id;
+  String ip;
+  String model;
+  String modelName;
+  String name;
+  String networkType;
+  String resolution;
+  bool smartHubAgreement;
+  String ssid;
+  String type;
+  String udn;
+  String wifiMac;
+
+  Device({
+    required this.frameTVSupport,
+    required this.gamePadSupport,
+    required this.imeSyncedSupport,
+    required this.os,
+    required this.tokenAuthSupport,
+    required this.voiceSupport,
+    required this.countryCode,
+    required this.description,
+    required this.developerIP,
+    required this.developerMode,
+    required this.duid,
+    required this.firmwareVersion,
+    required this.id,
+    required this.ip,
+    required this.model,
+    required this.modelName,
+    required this.name,
+    required this.networkType,
+    required this.resolution,
+    required this.smartHubAgreement,
+    required this.ssid,
+    required this.type,
+    required this.udn,
+    required this.wifiMac,
+  });
+
+  factory Device.fromJson(Map<String, dynamic> json) {
+    return Device(
+      frameTVSupport: json['FrameTVSupport'] == 'true',
+      gamePadSupport: json['GamePadSupport'] == 'true',
+      imeSyncedSupport: json['ImeSyncedSupport'] == 'true',
+      os: json['OS'],
+      tokenAuthSupport: json['TokenAuthSupport'] == 'true',
+      voiceSupport: json['VoiceSupport'] == 'true',
+      countryCode: json['countryCode'],
+      description: json['description'],
+      developerIP: json['developerIP'],
+      developerMode: json['developerMode'],
+      duid: json['duid'],
+      firmwareVersion: json['firmwareVersion'],
+      id: json['id'],
+      ip: json['ip'],
+      model: json['model'],
+      modelName: json['modelName'],
+      name: json['name'],
+      networkType: json['networkType'],
+      resolution: json['resolution'],
+      smartHubAgreement: json['smartHubAgreement'] == 'true',
+      ssid: json['ssid'],
+      type: json['type'],
+      udn: json['udn'],
+      wifiMac: json['wifiMac'],
+    );
+  }
+}
+
+class Tv {
+  final String name;
+  final String model;
+  final String id;
+  final String ip;
+  final String wifiMac;
+
+  Tv({
+    required this.name,
+    required this.model,
+    required this.id,
+    required this.ip,
+    required this.wifiMac,
+  });
+}
 
 main() async {
   var urn = 'urn:samsung.com:device:RemoteControlReceiver:1';
@@ -10,12 +144,12 @@ main() async {
 
 class TvCollector {
   final String urn;
-  final Map<String, String> headers = {'Accept': 'application/xml'};
+  final Map<String, String> headers = {'Accept': 'application/json'};
 
   TvCollector(this.urn);
 
   collect() async {
-    List<Map<String, Object>> tvs = [];
+    List<Tv> tvs = [];
     var list = await Discover(urn).search();
 
     for (var location in list) {
@@ -23,22 +157,18 @@ class TvCollector {
         continue;
       }
 
-      final uri = Uri.parse(location);
+      final uri = Uri.parse(location).replace(port: 8001, path: '/api/v2/');
       final response = await http.get(uri, headers: headers);
-      final rcrDoc = XmlDocument.parse(response.body);
-      final name =
-          rcrDoc.findAllElements('friendlyName').map((node) => node.text).first;
-      final model =
-          rcrDoc.findAllElements('modelName').map((node) => node.text).first;
-      final uuid = rcrDoc.findAllElements('UDN').map((node) => node.text).first;
-
-      tvs.add({
-        'name': name,
-        'model': model,
-        'uuid': uuid,
-        'host': uri.host,
-        'port': uri.port
-      });
+      DeviceInfo deviceInfo = DeviceInfo.fromJson(json.decode(response.body));
+      final device = deviceInfo.device;
+      Tv tv = Tv(
+        name: device.name,
+        model: device.modelName,
+        id: device.id,
+        ip: device.ip,
+        wifiMac: device.wifiMac,
+      );
+      tvs.add(tv);
     }
 
     return tvs;
