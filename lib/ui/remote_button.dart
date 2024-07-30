1a59c6990ee1a0af438dad7e3186547437eb0435
Sergey Bekharsky <bekharsky@gmail.com>
2024-07-30 19:44:25 +0300

    animation

--- lib/ui/remote_button.dart
+++ lib/ui/remote_button.dart
@@ -1,110 +1,31 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:remote/ui/remote_tap.dart';
 
-class RemoteButton extends StatefulWidget {
+class RemoteButton extends StatelessWidget {
   final Widget child;
-  final Function onPressed;
+  final void Function() onPressed;
   final double size;
-  final Color startColor = const Color.fromRGBO(73, 73, 73, 1);
-  final Color endColor = const Color.fromRGBO(255, 152, 0, 1);
 
-  RemoteButton({
+  const RemoteButton({
     Key? key,
     this.size = 48,
-    required this.child,
     required this.onPressed,
-  });
+    required this.child,
+  }) : super(key: key);
 
-  @override
-  _ColorChangeButtonState createState() => _ColorChangeButtonState();
-}
-
-class _ColorChangeButtonState extends State<RemoteButton>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Color?> _colorTween;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: Duration(milliseconds: 150),
-      vsync: this,
-    );
-    _colorTween = ColorTween(begin: widget.startColor, end: widget.endColor)
-        .animate(_controller);
-  }
+  final BoxDecoration _style = const BoxDecoration(
+    borderRadius: BorderRadius.all(Radius.circular(9999)),
+    color: Color.fromRGBO(73, 73, 73, 1),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        _controller.forward();
-        Future.delayed(const Duration(milliseconds: 150), () {
-          _controller.reverse();
-        });
-        widget.onPressed();
-      },
-      child: AnimatedBuilder(
-        animation: _colorTween,
-        builder: (context, child) {
-          return ElevatedButton(
-            onPressed: null,
-            style: ElevatedButton.styleFrom(
-              disabledForegroundColor: Colors.white,
-              disabledBackgroundColor: _colorTween.value,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(9999),
-              ),
-              padding: EdgeInsets.all(16),
-            ),
-            child: widget.child,
-          );
-        },
-      ),
+    return RemoteTap(
+      width: size,
+      height: size,
+      style: _style,
+      onPressed: onPressed,
+      child: child,
     );
   }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 }
-
-// import 'package:flutter/widgets.dart';
-// import 'package:remote/ui/remote_tap.dart';
-
-// class RemoteButton extends StatelessWidget {
-//   final Widget child;
-//   final void Function() onPressed;
-//   final double size;
-
-//   const RemoteButton({
-//     Key? key,
-//     this.size = 48,
-//     required this.onPressed,
-//     required this.child,
-//   }) : super(key: key);
-
-//   final BoxDecoration _defaultStyle = const BoxDecoration(
-//     borderRadius: BorderRadius.all(Radius.circular(9999)),
-//     color: Color.fromRGBO(73, 73, 73, 1),
-//   );
-
-//   final BoxDecoration _pressedStyle = const BoxDecoration(
-//     borderRadius: BorderRadius.all(Radius.circular(9999)),
-//     color: Color.fromRGBO(255, 152, 0, 1),
-//   );
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return RemoteTap(
-//       width: size,
-//       height: size,
-//       defaultStyle: _defaultStyle,
-//       pressedStyle: _pressedStyle,
-//       onPressed: onPressed,
-//       child: child,
-//     );
-//   }
-// }
--- lib/ui/remote_tap.dart
+++ lib/ui/remote_tap.dart
@@ -5,8 +5,9 @@ class RemoteTap extends StatefulWidget {
   final double width;
   final double height;
   final void Function() onPressed;
-  final BoxDecoration defaultStyle;
-  final BoxDecoration pressedStyle;
+  final BoxDecoration style;
+  final Color startColor = const Color.fromRGBO(73, 73, 73, 1);
+  final Color endColor = const Color.fromRGBO(255, 152, 0, 1);
   final Widget child;
 
   const RemoteTap({
@@ -14,8 +15,7 @@ class RemoteTap extends StatefulWidget {
     required this.width,
     required this.height,
     required this.onPressed,
-    this.defaultStyle = const BoxDecoration(),
-    this.pressedStyle = const BoxDecoration(),
+    this.style = const BoxDecoration(),
     this.child = const SizedBox.shrink(),
   }) : super(key: key);
 
@@ -23,44 +23,52 @@ class RemoteTap extends StatefulWidget {
   State<RemoteTap> createState() => _RemoteTapState();
 }
 
-class _RemoteTapState extends State<RemoteTap> {
-  bool _pressed = false;
+class _RemoteTapState extends State<RemoteTap>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Color?> _colorTween;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 150),
+      vsync: this,
+    );
+    _colorTween = ColorTween(begin: widget.startColor, end: widget.endColor)
+        .animate(_controller);
+  }
 
   void _handleTapDown(TapDownDetails details) {
-    setState(() {
-      _pressed = true;
+    _controller.forward();
+
+    Future.delayed(const Duration(milliseconds: 150), () {
+      _controller.reverse();
     });
 
     // TODO: settings
     HapticFeedback.mediumImpact();
-  }
-
-  void _handleTapUp(TapUpDetails details) {
-    setState(() {
-      _pressed = false;
-    });
 
     widget.onPressed();
-  }
-
-  void _handleTapCancel() {
-    setState(() {
-      _pressed = false;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTapDown: _handleTapDown,
-      onTapUp: _handleTapUp,
-      onTapCancel: _handleTapCancel,
-      child: Container(
-        width: widget.width,
-        height: widget.height,
-        decoration: _pressed ? widget.pressedStyle : widget.defaultStyle,
-        child: widget.child,
-      ),
+      child: AnimatedBuilder(
+          animation: _colorTween,
+          builder: (context, child) {
+            return Container(
+              width: widget.width,
+              height: widget.height,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(9999)),
+                color: _colorTween.value,
+              ),
+              child: widget.child,
+            );
+          }),
     );
   }
 }
