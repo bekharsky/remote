6734286746bcd3ddba0045a3eccbcaa2e141c44a
Sergey Bekharsky <bekharsky@gmail.com>
2025-05-15 12:53:27 +0300

    blink skeletons

--- lib/ui/remote_apps_skeleton.dart
+++ lib/ui/remote_apps_skeleton.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/cupertino.dart';
 
-class RemoteAppsSkeleton extends StatelessWidget {
+class RemoteAppsSkeleton extends StatefulWidget {
   const RemoteAppsSkeleton({
     super.key,
   });
+
+  @override
+  State<RemoteAppsSkeleton> createState() => _RemoteAppsSkeletonState();
+}
+
+class _RemoteAppsSkeletonState extends State<RemoteAppsSkeleton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _blinkController;
+  late final Animation<double> _blinkAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the blink animation
+    _blinkController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 600), // Slow blinking
+    )..repeat(reverse: true);
+
+    _blinkAnimation = Tween(begin: 0.4, end: 1.0).animate(CurvedAnimation(
+      parent: _blinkController,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _blinkController.dispose(); // Dispose the controller to avoid memory leaks
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +49,21 @@ class RemoteAppsSkeleton extends StatelessWidget {
             right: 12,
           );
 
-          return Container(
-            margin: itemMargin,
-            width: 120, // Approximate width of an app icon
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(100, 100, 100, 1.0),
-              borderRadius: BorderRadius.circular(16),
-            ),
+          return AnimatedBuilder(
+            animation: _blinkAnimation,
+            builder: (context, child) {
+              return Opacity(
+                opacity: _blinkAnimation.value,
+                child: Container(
+                  margin: itemMargin,
+                  width: 120, // Approximate width of an app icon
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(120, 120, 120, 1.0),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              );
+            },
           );
         },
       ),
